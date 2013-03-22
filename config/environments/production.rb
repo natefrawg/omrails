67e4b42d663e63b8a1fda9aa036d2319e956ece3
Nathan Schuett <nschuett@gmail.com>
2013-03-21 23:40:14 -0700

    Get AWS S3 working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,9 @@ Omrails::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => "twinterest",
+      :access_key_id => "AKIAJLFTYCAUVGHUSSQQ",
+      :secret_access_key => "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"
     }
   }
 
